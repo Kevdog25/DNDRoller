bcf1b8960abeb04e0eb6d0cb606932fbc454943d
Kevin Welsh <kevinwelsh132@gmail.com>
2018-12-20 18:57:24 -0600

    Aded a thign

--- Character.py
+++ Character.py
@@ -5,5 +5,14 @@ class Character:
         self.Name = random.choice(['Kevin', 'Alice', 'Dri', 'J'])
         pass
 
+    @classmethod
+    def fromJson(cls, jsonObj):
+        '''Takes a JSON object, parses it and returns a character object'''
+        character = Character()
+        # TODO - Actually do this.
 
-    
+        return character
+
+if __name__ == '__main__':
+    print('You can test things out here.')
+    print('Try running \"python Character.py\" from the right location in a cmd prompt')
