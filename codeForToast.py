cb3a060fc28b26daf19d8047a0fac6c7d3736358
Kevin Welsh <kevinwelsh132@gmail.com>
2018-12-31 20:36:31 -0600

    real code

--- codeForToast.py
+++ codeForToast.py
@@ -1,14 +1,5 @@
-#from Character import Character
+from Character import Character
 from Skills import Skills
-import random
-
-class Character:
-    Name = 'test'
-    def isProficient(self, skill):
-        return random.choice([True, False])
-    @classmethod
-    def fromJson(cls,f):
-        return [Character() for _ in range(3)]
 
 if __name__ == '__main__':
     characterList = Character.fromJson('characters.json')
@@ -16,5 +7,6 @@ if __name__ == '__main__':
         print(c.Name)
         print('Proficiencies:')
         for s in Skills:
-            print(f'{s.name} : {c.isProficient(s)}')
+            print(f'\t{s.name} : {c.isProficient(s)}')
+        print()
             
