613687541223e9b538c9e268581c0a9b2ff708b1
Kevin Welsh <kevinwelsh132@gmail.com>
2019-01-06 18:41:39 -0600

    Alice's changes

--- DNDRoller/Character.py
+++ DNDRoller/Character.py
@@ -1,18 +1,50 @@
-import random
+import json
+from Skills import Skills
 
 class Character:
     def __init__(self):
-        self.Name = random.choice(['Kevin', 'Alice', 'Dri', 'J'])
         pass
 
-    @classmethod
-    def fromJson(cls, jsonObj):
-        '''Takes a JSON object, parses it and returns a character object'''
-        character = Character()
-        # TODO - Actually do this.
+    def isProficient(self, skill):
+            return skill in self.Proficiencies
+    
+    def isExpert(self, skill):
+            return skill in self.Expertise
 
-        return character
+    @classmethod
+    def fromJson(cls, json_file):
+        '''Takes a JSON object, parses it and returns a character object'''
+        with open(json_file, 'r') as fin:
+            data = json.load(json_data)
+        characterList = []
+        for k in data:
+            character = Character()
+            character.Name = k["Name"]
+            character.Strength = k["Stats"]["Strength"]
+            character.Dexterity = k["Stats"]["Dexterity"]
+            character.Constitution = k["Stats"]["Constitution"]
+            character.Wisdom = k["Stats"]["Wisdom"]
+            character.Intelligence = k["Stats"]["Intelligence"]
+            character.Charisma = k["Stats"]["Charisma"]
+            character.Proficiencies = []
+            for p in list(k['isProficient']):
+                character.Proficiencies.append(Skills[p])
+            character.Expertise = []
+            for e in list(k['isExpert']):
+                character.Expertise.append(Skills[e])
+            characterList.append(character)
+        return characterList
 
 if __name__ == '__main__':
     print('You can test things out here.')
     print('Try running \"python Character.py\" from the right location in a cmd prompt')
+    #Alice = Character.getCharacter("Alice")
+    characterList = Character.fromJson('character.json')
+    for c in characterList:
+        print(c.Name)
+        print('Proficiencies:')
+        for s in Skills:
+            print(f'\t{s.name} : {c.isProficient(s)}')
+        print()
+        print(c.Proficiencies)
+        print(c.Expertise)
